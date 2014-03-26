5b5252e5e09eee5feae1ea612cd7723d545f84cb
George Spake <gpspake@gmail.com>
2014-03-25 23:10:19 -0500

    Create gs.sh
    Create file and add script

---
+++ gs.sh
@@ -0,0 +1,20 @@
+echo 'cd to opt dir'
+cd /opt
+echo 'wget node.js'
+#sudo wget http://nodejs.org/dist/node-latest.tar.gz
+#echo 'extract'
+#sudo tar -xzvf node-latest.tar.gz
+echo 'cd in to node dir'
+cd node-v*
+echo 'configure'
+sudo ./configure
+echo 'make'
+sudo make
+echo 'install'
+sudo make install
+echo 'adding /usr/local/bin to path'
+export PATH=$PATH:/usr/local/bin
+echo 'node version'
+node -v
+echo 'npm version'
+npm -v
